bd2ee2c86416cde158f50b38022211e257de0c82
Silas Augusto <augusto.fernandes775@gmail.com>
2020-05-07 16:45:03 -0300

    configuration sequelize
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,10 @@
 const routes = require('express').Router()
+const { User } = require('./app/models')
+
+User.create({ 
+    name: 'Diego', 
+    email: 'augusto.fernandes775@gmail.com',
+    password_hash: '16461651861651661'
+})
 
 module.exports = routes
