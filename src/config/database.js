192a6147cc364a9bfd8c220961a352bf7cc059c7
Silas Augusto <augusto.fernandes775@gmail.com>
2020-05-07 17:51:15 -0300

    configuration jest
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,14 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : 
+})
+
 module.exports = {
-  host: '127.0.0.1',
-  username: 'postgres',
-  password: '123456',
-  database: 'nodeauth',
-  dialect: 'postgres',
+  host: process.env.BD_HOST,
+  username: process.env.BD_USER,
+  password: process.env.BD_PASS,
+  database: process.env.BD_NAME,
+  dialect: process.env.BD_DIALECT || 'postgres',
+  storage: './__tests__/database.sqlite',
   operatorAliases: false,
   logging: false,
   define: {
