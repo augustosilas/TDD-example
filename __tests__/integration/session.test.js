40729e0adbfc06873aee7fb6def9f78189e92fae
Silas Augusto <augusto.fernandes775@gmail.com>
2020-05-08 18:20:19 -0300

    applying TDD, auth users
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest')
 
 const app = require('../../src/app')
-const { User } = require('../../src/app/models')
 const truncate = require('../utils/truncate')
+const factory = require('../factories')
 
 describe('Authentication', () => {
     beforeEach(async () => {
@@ -10,10 +10,23 @@ describe('Authentication', () => {
     })
 
     it('should authenticate with valid credentials', async () => {
-        const user = await User.create({
-            name: 'Silas',
-            email: 'augusto_fernandes75@hotmail.com',
-            password_hash: '123123'
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123123'
+            })
+
+        expect(response.status).toBe(200);
+    });
+
+    it('should not authenticate with invalid credentials', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
         })
 
         const response = await request(app)
@@ -23,6 +36,47 @@ describe('Authentication', () => {
                 password: '123456'
             })
 
+        expect(response.status).toBe(401);
+    })
+
+    it('should return jwt token when authenticated', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123123'
+            })
+
+        expect(response.body).toHaveProperty('token');
+    })
+
+    it('should be able to acess private routes when authenticated', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer ${user.generateToken()}`)
+            
         expect(response.status).toBe(200);
-    });
+    })
+
+    it('should not be able to acess private routes without jwt token', async () => {
+        const response = await request(app).get('/dashboard')
+            
+        expect(response.status).toBe(401);
+    })
+
+    it('should not be able to acess private routes with invalid jwt token', async () => {
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer 123123`)
+        
+        expect(response.status).toBe(401);
+    })
 })
